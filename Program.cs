3f087ecfe47134129d23709b1572bf7c890750cb
yavuz06 <yavuzselmkaya@gmail.com>
2023-03-12 19:09:10 +0300

    week3part2

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+int weekday = Convert.ToInt32(DateTime.Now.DayOfWeek);
+Console.WriteLine(weekday);
+
+switch (weekday)
+
+{
+ case 0:
+        Console.WriteLine("Sunday");
+break; 
+    case 1:
+        Console.WriteLine("Monday");
+        break;
+        case 2:
+        Console.WriteLine("Tuesday");
+        break;
+        case 3:
+        Console.WriteLine("Wednesday");
+        break;
+        case 4:
+        Console.WriteLine("Thursday");
+        break;
+    case 5:
+        Console.WriteLine("Friday");
+            break;
+        case 6:
+        Console.WriteLine("Saturday");
+        break;
+
+        default: Console.WriteLine("ınvalid value");
+        break;
+
+}
